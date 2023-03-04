b7d994145b51ccb4fe7dfbbe84e10d37989f54df
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2023-03-04 23:41:20 +0900

    feature/mizumashi 水増しスクリプト着手

---
+++ augument.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import glob
+
+N_img = 20 # 1枚当たりの水増し枚数
+
+input_path_ok = "./train_images/ok/*"
+ok_files = glob.glob(input_path_ok)
+
+output_path_ok = "./train_images/ok_augumented/"
+if os.path.isdir(output_path_ok) == True:
+    os.removedirs(output_path_ok)
+os.mkdir(output_path_ok)
+
+input_path_dangerous = "./train_images/dangerous/*"
+dangerous_file = glob.glob(input_path_dangerous)
+
+output_path_dangerous = "./train_images/dangerous_augumented/"
+if os.path.isdir(output_path_dangerous) == True:
+    os.remove(output_path_dangerous)
+os.mkdir(output_path_dangerous)
