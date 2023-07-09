e87dabd3ed05b4ff2c3accd5a4b39de037299a52
gmimaki <gmimakiwithkamejiroandnana@gmail.com>
2023-07-09 13:16:02 +0900

    feature/sns SMSログを残す

--- infra/modules/sns/main.tf
+++ infra/modules/sns/main.tf
@@ -8,3 +8,49 @@ resource "aws_sns_topic_subscription" "tortoise_safe_checker" {
   protocol = "sms"
   endpoint = var.phone_number
 }
+
+resource "aws_sns_sms_preferences" "tortoise_safe_checker" {
+  delivery_status_iam_role_arn = aws_iam_role.SNS_SMS_tortoise_safe_checker.arn
+}
+
+resource "aws_iam_role" "SNS_SMS_tortoise_safe_checker" {
+  name = "SNS_SMS_tortoise_safe_checker"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "SNS_SMS_tortoise_safe_checker" {
+  name = "SNS_SMS_tortoise_safe_checker"
+  role = aws_iam_role.SNS_SMS_tortoise_safe_checker.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
