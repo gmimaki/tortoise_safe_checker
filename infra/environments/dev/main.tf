782f1e49c350c6890bf7da1b9d5df2852deb95dd
gmimaki <gmimakiwithkamejiroandnana@gmail.com>
2023-08-19 15:20:57 +0900

    feature/sqs_and_lambda SQSにルーティング

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -10,19 +10,19 @@ module "ecr" {
   source = "../../modules/ecr"
 }
 
-module "dynamodb" {
-  source = "../../modules/dynamodb"
-}
+#module "dynamodb" {
+#  source = "../../modules/dynamodb"
+#}
 
-module "sns" {
-  source        = "../../modules/sns"
-  phone_number  = var.phone_number
-}
+#module "sns" {
+#  source        = "../../modules/sns"
+#  phone_number  = var.phone_number
+#}
 
 module "sqs" {
   source     = "../../modules/sqs"
   account_id = var.account_id
-  sns_topic_arn = module.sns.topic_arn
+  #sns_topic_arn = module.sns.topic_arn
 }
 
 #module "ses" {
@@ -30,20 +30,22 @@ module "sqs" {
 #  email = var.sender_email
 #}
 
-module "lambda" {
-  source = "../../modules/lambda"
+#module "lambda" {
+  #source = "../../modules/lambda"
   #sender_email = var.sender_email
   #receipient_email = var.receipient_email
-  sns_topic_arn       = module.sns.topic_arn
-  ecr_image_uri       = module.ecr.repository_url
-  ecr_image_arn       = module.ecr.repository_arn
-  dynamodb_stream_arn = module.dynamodb.dynamodb_stream_arn
-}
+  #sns_topic_arn       = module.sns.topic_arn
+  #ecr_image_uri       = module.ecr.repository_url
+  #ecr_image_arn       = module.ecr.repository_arn
+  #dynamodb_stream_arn = module.dynamodb.dynamodb_stream_arn
+#}
 
 module "iot_core" {
   source = "../../modules/iot_core"
+  sqs_queue_arn = module.sqs.sqs_queue_arn
+  sqs_queue_url = module.sqs.sqs_queue_url
   #kinesis_stream_arn  = module.kinesis.stream_arn
   #kinesis_stream_name = module.kinesis.stream_name
-  dynamodb_table_arn  = module.dynamodb.dynamodb_table_arn
-  dynamodb_table_name = module.dynamodb.dynamodb_table_name
+  #dynamodb_table_arn  = module.dynamodb.dynamodb_table_arn
+  #dynamodb_table_name = module.dynamodb.dynamodb_table_name
 }
--- infra/modules/sqs/main.tf
+++ infra/modules/sqs/main.tf
@@ -14,33 +14,25 @@ resource "aws_sqs_queue" "tortoise_safe_checker_dlq" {
   })
 }
 
-resource "aws_sns_topic_subscription" "tortoise_safe_checker_sqs" {
-  topic_arn = var.sns_topic_arn
-  protocol = "sqs"
-  endpoint = aws_sqs_queue.tortoise_safe_checker.arn
+#resource "aws_sns_topic_subscription" "tortoise_safe_checker_sqs" {
+#  topic_arn = var.sns_topic_arn
+#  protocol = "sqs"
+#  endpoint = aws_sqs_queue.tortoise_safe_checker.arn
+#}
+
+data "aws_iam_policy_document" "sqs_policy" {
+  statement {
+    actions = ["sqs:SendMessage"]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["iot.amazonaws.com"]
+    }
+    resources = [aws_sqs_queue.tortoise_safe_checker.arn]
+  }
 }
 
 resource "aws_sqs_queue_policy" "tortoise_safe_checker" {
   queue_url = aws_sqs_queue.tortoise_safe_checker.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "MyQueuePolicy",
-  "Statement": [
-    {
-      "Sid": "Allow-SNS-Messages",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.tortoise_safe_checker.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${var.sns_topic_arn}"
-        }
-      }
-    }
-  ]
-}
-POLICY
+  policy = data.aws_iam_policy_document.sqs_policy.json
 }
